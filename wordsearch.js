51fb36c06423dbf55fbf652aaa8948216cbca2f2
Navya Rajput <navyasrajput@gmail.com>
2021-09-22 22:07:29 -0400

    word search in 2d array

--- wordsearch.js
+++ wordsearch.js
@@ -1,9 +1,56 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+
+    if (!Array.isArray(letters) || !letters.length)
+       return false;
+
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+   
+//    console.log("letters: " + letters);
+//    console.log("word: " + word);
+//    console.log("hz: " + horizontalJoin);
+    if (horizontalJoin.includes(word)) {
+     //   console.log("Found:" + word);
+return true
     }
+    for (let i = 0; i < letters[i].length; i++) {
+    let verticalWord = '';
+for (let j = 0; j < letters.length; j++) {
+        verticalWord += letters[j][i];
+}
+    if (verticalWord.includes(word)) {
+              //  console.log("Found:" + word);
+          return true;
+}
+    }
+    return false;
+
 }
 
+/*
 
+    wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+    wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEW')
+
+*/
 module.exports = wordSearch
+
